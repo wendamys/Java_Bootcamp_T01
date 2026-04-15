021de2ea3ccf716cb9dece2e9cc4aef9923da89d
wendamys <wendamys@yandex.ru>
2026-04-15 14:17:57 +0700

    rename exercise0.java -> exercise1.java

--- src/main/java/org/example/exercise1.java
+++ src/main/java/org/example/exercise1.java
@@ -3,7 +3,7 @@ import java.lang.Math;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class exercise0 {
+public class exercise1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
